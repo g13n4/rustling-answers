746e72e9abe58eabd8fdb4078e71a6f650b2e899
mx <joey-hzpfywtd@hotmail.com>
2024-08-03 22:29:37 +0300

    Add solutions

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -3,25 +3,24 @@
 
 #[allow(dead_code)]
 mod delicious_snacks {
-    // TODO: Add the following two `use` statements after fixing them.
-    // use self::fruits::PEAR as ???;
-    // use self::veggies::CUCUMBER as ???;
-
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
 }
 
+pub use crate::delicious_snacks::fruits::PEAR as fruit;
+pub use crate::delicious_snacks::veggies::CUCUMBER as veggie;
+
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie,
+        fruit,
+        veggie,
     );
 }
